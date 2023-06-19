c610dfd97203853a4da4668ad8f161ff4a2b050d
Hamid Alinia <a.goodlookingboy@gmail.com>
2023-06-19 12:55:22 +0330

    add my transactions

--- node_modules/persianjs/test/test.persian.js
+++
@@ -1,9 +0,0 @@
-//Prepare test
-require('./../persian');
-var assert = require('assert');
-describe('convert', function () {
-	it('should Convert Persian numbers to English', function (done) {
-		assert.equal("1234567890", persianJs("۱۲۳۴۵۶۷۸۹۰").persianNumber().toString());
-        done();
-    });
-});
--- node_modules/persianjs/test/test.toEnglish.js
+++
@@ -1,9 +0,0 @@
-//Prepare test
-require('./../persian');
-var assert = require('assert');
-describe('convert', function () {
-	it('should Convert Persian and Arabic numbers to English', function (done) {
-		assert.equal("12345678901234567890", persianJs("۱۲۳۴۵۶۷۸۹۰١٢٣٤٥٦٧٨٩٠").toEnglishNumber().toString());
-        done();
-    });
-});
